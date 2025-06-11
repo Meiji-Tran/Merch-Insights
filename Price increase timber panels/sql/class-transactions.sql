1b0c16fe3168a500b3159b5a29eab1357d9ce5c5
Meiji-Tran <meiji.tran1@bunnings.com.au>
2025-06-11 05:54:40 +0000

    AU class-based

---
+++ Price increase timber panels/sql/class-transactions.sql
@@ -0,0 +1,28 @@
+SELECT
+    DATE_TRUNC('WEEK', stl.TRANSACTION_DATE) AS week_start,
+    i.item_number,
+    i.item_description,
+    i.item_class_name,
+    i.item_sub_class_name,
+    
+    SUM(stl.TOTAL_EXCLUDE_GST_AMOUNT) AS total_sales,
+    SUM(stl.SALES_QUANTITY) AS total_quantity,
+    COUNT(DISTINCT stl.dw_loyalty_flybuys_account_id) AS unique_customers
+
+FROM bdwprd_cds.sales.sales_transaction_line_fct stl
+JOIN bdwprd_cds.item.item_dim i
+    ON stl.dw_item_id = i.dw_item_id
+
+WHERE 1=1
+    AND stl.TRANSACTION_DATE BETWEEN {start_date} AND {end_date}
+    AND i.item_class_name IN ('500 PANELS', '500 PANELS BULK STACK')
+    AND i.item_department_name != '.Unk'
+    AND stl.sales_reporting_include_ind = TRUE
+    AND stl.dw_loyalty_flybuys_account_id != MD5_BINARY(-1)
+    AND stl.customer_type_code = 'Consumer'
+    AND stl.country_code = {country}
+
+GROUP BY
+    1, 2, 3, 4, 5
+ORDER BY
+    week_start, item_number;
