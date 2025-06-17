679a3c437f59333f4aa0ceb893d41a06b919b150
Meiji-Tran <meiji.tran1@bunnings.com.au>
2025-06-17 06:22:39 +0000

    skylight set up

--- Skylight cross shop/sql/sql/excluded_class_list.sql
+++ Skylight cross shop/sql/sql/excluded_class_list.sql
@@ -4,21 +4,8 @@ SELECT DISTINCT
     ITEM_CLASS_NAME
             FROM bdwprd_cds.item.item_dim i
         WHERE 1=1
-                and i.item_department_name = '300 LIGHTING'
+                and i.item_sub_department_name = '400 SKYLIGHTS AND ROOF WINDOWS'
                 and ITEM_CLASS_NAME not in (
-                '503 IND LIGHTING INSTALLATION',
-                '502 FAN INSTALLATION'
+                '500 SKYLIGHT ACCESSORIES',
+                '500 ROOF WINDOWS'
                 ) 
-    -- change below to 'not in' for functional lighting. Or 'in' for decorative lighting. Comment whole block out for overall lighting
-     --           and ITEM_CLASS_NAME in (
-     --           '502 DECORATIVE',
-     --           '502 G SERIES',
-     --           '502 FLOOR LAMPS',
-     --           '502 MIX AND MATCH LAMPS',
-     --           '502 MIX AND MATCH SHADES',
-     --           '502 TABLE LAMPS',
-     --           '502 CHANDELIER',
-     --           '502 COMPLETE 240V PENDANT',
-     --           '502 DIY PENDANT',
-     --           '502 SUSPENSION',
-     --           '502 SOLAR DECORATIVE')
--- Skylight cross shop/sql/sql/item_association.sql
+++ Skylight cross shop/sql/sql/item_association.sql
@@ -1,6 +1,7 @@
-select primary_id, secondary_id, primary_transactions,SECONDARY_TRANSACTIONS,TRX_START_MONTH,TRX_END_MONTH,pair_transactions,LIFT,JACCARD from BDWPRD_DE.IA_MERCH_DE.LIGHTING_CLASS_TO_CLASS_ASSOCIATION_METRICS 
+select primary_id, secondary_id, primary_transactions,SECONDARY_TRANSACTIONS,TRX_START_MONTH,TRX_END_MONTH,pair_transactions,LIFT,JACCARD 
+from BDWPRD_DE.IA_MERCH_DE.SKYLIGHT_CLASS_TO_CLASS_ASSOCIATION_METRICS 
 where 1=1
-and customer_type = 'All'
-and country_code ='AU'
+and customer_type = 'Commercial'
+and country_code ='NZ'
 AND MODEL_TYPE = 'Basket'
 QUALIFY ROW_NUMBER() OVER (PARTITION BY concat(primary_id, secondary_id,customer_type,model_type,country_code) ORDER BY TRX_START_MONTH DESC) = 1
