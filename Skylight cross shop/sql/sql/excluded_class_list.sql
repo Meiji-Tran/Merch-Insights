c61b840873d4c97f3951a99d7b2ad372ae270325
Meiji-Tran <meiji.tran1@bunnings.com.au>
2025-06-17 08:44:03 +0000

    rerun

--- Skylight cross shop/sql/sql/excluded_class_list.sql
+++ Skylight cross shop/sql/sql/excluded_class_list.sql
@@ -1,11 +1,6 @@
-
 SELECT DISTINCT 
     --ITEM_SUB_DEPARTMENT_NAME
     ITEM_CLASS_NAME
             FROM bdwprd_cds.item.item_dim i
         WHERE 1=1
-                and i.item_sub_department_name = '400 SKYLIGHTS AND ROOF WINDOWS'
-                and ITEM_CLASS_NAME not in (
-                '500 SKYLIGHT ACCESSORIES',
-                '500 ROOF WINDOWS'
-                ) 
+                and i.item_sub_department_name = '400 SKYLIGHTS AND ROOF WINDOWS'; 
