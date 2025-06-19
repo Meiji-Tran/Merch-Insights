d438da9ea6817554fdea3d49592f9535c34d831f
Meiji-Tran <meiji.tran1@bunnings.com.au>
2025-06-19 00:54:00 +0000

    Skylight overview

--- Skylight cross shop/sql/sql/excluded_class_list.sql
+++ Skylight cross shop/sql/sql/excluded_class_list.sql
@@ -3,4 +3,8 @@ SELECT DISTINCT
     ITEM_CLASS_NAME
             FROM bdwprd_cds.item.item_dim i
         WHERE 1=1
-                and i.item_sub_department_name = '400 SKYLIGHTS AND ROOF WINDOWS'; 
+                and i.item_sub_department_name = '400 SKYLIGHTS AND ROOF WINDOWS'
+                and i.item_class_name in (
+                '500 ROOF WINDOWS',
+                '500 SKYLIGHT ACCESSORIES'
+                ); 
---
+++ Skylight cross shop/sql/sql/overview.sql
@@ -0,0 +1,27 @@
+SELECT 
+    CASE WHEN GROUPING(i.item_number) = 1 THEN 'TOTAL' ELSE i.item_number END AS item_number,
+    CASE WHEN GROUPING(i.item_description) = 1 THEN 'TOTAL' ELSE i.item_description END AS item_description,
+    COUNT(DISTINCT stl.dw_sales_transaction_id) AS transactions,
+    ROUND(SUM(stl.total_exclude_gst_amount)) AS sales_sum,
+    ROUND(SUM(stl.sales_quantity)) AS quantity_sum
+FROM bdwprd_cds.sales.sales_transaction_line_fct stl
+INNER JOIN bdwprd_cds.item.item_dim i
+    ON stl.dw_item_id = i.dw_item_id
+INNER JOIN bdwprd_cds.merchandising.item_supplier_cost_price_fct isc
+    ON i.dw_item_id = isc.dw_item_id
+INNER JOIN bdwprd_cds.supplier.supplier_dim s
+    ON s.dw_supplier_id = isc.dw_supplier_id
+WHERE 
+    stl.country_code = 'NZ'
+    AND stl.sales_reporting_include_ind = TRUE
+    AND stl.transaction_date BETWEEN '2022-06-18' AND '2025-06-18'
+    AND stl.customer_type_code = 'Commercial'
+    AND stl.dw_commercial_account_id != MD5_BINARY(-1)
+    AND s.supplier_code = '4913000'
+GROUP BY GROUPING SETS (
+    (i.item_number, i.item_description),
+    ()
+)
+ORDER BY 
+    sales_sum DESC;
+
