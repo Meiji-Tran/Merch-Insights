d3f820885273d4d4633aff7c4e321d2bf15bc580
Meiji-Tran <meiji.tran1@bunnings.com.au>
2025-06-19 11:55:09 +0000

    AU profiling

--- NZ pest control & growing media/sql/2-diy-proficiency.sql
+++ NZ pest control & growing media/sql/2-diy-proficiency.sql
@@ -1,49 +1,128 @@
--- DIY proficiency
+-- DIY Proficiency Indexing by {level} and Proficiency Group
+
+WITH diy_seg AS (
+    SELECT
+        household AS flybuys_membership_number_hash,
+        SUBSTRING(proficiency_group , 3) as proficiency_group
+    FROM dss_private_design.adhoc.diy_proficiency_audience
+),
+
+-- Step 1: All transactions for items in range
+range_trx AS (
+    SELECT
+        i.{level},
+        ds.proficiency_group,
+        stl.dw_loyalty_flybuys_account_id AS customer_id,
+        stl.total_exclude_gst_amount AS sales,
+        stl.item_quantity AS quantity
+    FROM bdwprd_cds.sales.sales_transaction_line_fct stl
+    JOIN bdwprd_de.ia_merch_de.{table_name} ir
+        ON stl.dw_item_id = ir.dw_item_id
+        AND stl.country_code = ir.country_code
+    JOIN bdwprd_cds.item.item_dim i
+        ON stl.dw_item_id = i.dw_item_id
+    JOIN bdwprd_cds.loyalty_master.loyalty_flybuys_account_dim fa
+        ON stl.dw_loyalty_flybuys_account_id = fa.dw_loyalty_flybuys_account_id
+    JOIN diy_seg ds
+        ON fa.flybuys_membership_number_hash = ds.flybuys_membership_number_hash
+    WHERE 1=1
+        AND stl.sales_reporting_include_ind = TRUE
+        AND stl.country_code = '{country}'
+        AND stl.transaction_date BETWEEN {start_date} AND {end_date}
+        AND stl.customer_type_code = 'Consumer'
+),
+
+-- Totals per {level} × proficiency group
+seg_level AS (
+    SELECT
+        {level},
+        proficiency_group,
+        SUM(sales) AS segment_sales,
+        SUM(quantity) AS segment_quantity,
+        COUNT(DISTINCT customer_id) AS segment_customers
+    FROM range_trx
+    GROUP BY {level}, proficiency_group
+),
+
+-- Totals per {level}
+total_level AS (
+    SELECT
+        {level},
+        SUM(sales) AS total_sales,
+        SUM(quantity) AS total_quantity,
+        COUNT(DISTINCT customer_id) AS total_customers
+    FROM range_trx
+    GROUP BY {level}
+),
+
+-- Totals per proficiency group
+seg_total AS (
+    SELECT
+        proficiency_group,
+        SUM(sales) AS total_seg_sales,
+        SUM(quantity) AS total_seg_quantity,
+        COUNT(DISTINCT customer_id) AS total_seg_customers
+    FROM range_trx
+    GROUP BY proficiency_group
+),
+
+-- Overall totals
+overall AS (
+    SELECT
+        SUM(sales) AS overall_sales,
+        SUM(quantity) AS overall_quantity,
+        COUNT(DISTINCT customer_id) AS overall_customers
+    FROM range_trx
+)
+
+-- Final output with indexes
 SELECT
-    i.item_class_name,
-    diy.proficiency_group,
-    -- Item range metrics
-    SUM(IFF(ir.item_number IS NOT NULL, stl.total_exclude_gst_amount, NULL)) AS range_sales,
-    SUM(IFF(ir.item_number IS NOT NULL, stl.item_quantity, NULL)) AS range_quantity,
-    COUNT(DISTINCT IFF(ir.item_number IS NOT NULL, stl.dw_loyalty_flybuys_account_id, NULL)) AS range_customers,
-    -- Item range shares
-    range_sales / SUM(range_sales) OVER () AS range_sales_share,
-    range_quantity / SUM(range_quantity) OVER () AS range_quantity_share,
-    range_customers / SUM(range_customers) OVER () AS range_customer_share,
-    -- Overall metrics
-    SUM(stl.total_exclude_gst_amount) AS overall_sales,
-    SUM(stl.item_quantity) AS overall_quantity,
-    COUNT(DISTINCT stl.dw_loyalty_flybuys_account_id) AS overall_customers,
-    -- Overall shares
-    overall_sales / SUM(overall_sales) OVER () AS overall_sales_share,
-    overall_quantity / SUM(overall_quantity) OVER () AS overall_quantity_share,
-    overall_customers / SUM(overall_customers) OVER () AS overall_customer_share,
+    sl.{level},
+    sl.proficiency_group,
+
+    sl.segment_sales,
+    sl.segment_quantity,
+    sl.segment_customers,
+
+    tl.total_sales,
+    tl.total_quantity,
+    tl.total_customers,
+
+    st.total_seg_sales,
+    st.total_seg_quantity,
+    st.total_seg_customers,
+
+    ovr.overall_sales,
+    ovr.overall_quantity,
+    ovr.overall_customers,
+
+    -- Shares in {level}
+    sl.segment_sales / NULLIF(tl.total_sales, 0) AS segment_sales_share,
+    sl.segment_quantity / NULLIF(tl.total_quantity, 0) AS segment_quantity_share,
+    sl.segment_customers / NULLIF(tl.total_customers, 0) AS segment_customer_share,
+
+    -- Shares overall
+    st.total_seg_sales / NULLIF(ovr.overall_sales, 0) AS overall_sales_share,
+    st.total_seg_quantity / NULLIF(ovr.overall_quantity, 0) AS overall_quantity_share,
+    st.total_seg_customers / NULLIF(ovr.overall_customers, 0) AS overall_customer_share,
+
     -- Indexes
-    range_sales_share / overall_sales_share AS sales_index,
-    range_quantity_share / overall_quantity_share AS quantity_index,
-    range_customer_share / overall_customer_share AS customer_index
-FROM bdwprd_cds.sales.sales_transaction_line_fct stl
-INNER JOIN bdwprd_cds.item.item_dim i
-    ON stl.dw_item_id = i.dw_item_id
-INNER JOIN bdwprd_cds.location.location_dim l
-    ON stl.dw_location_id = l.dw_location_id
-INNER JOIN bdwprd_cds.loyalty_master.loyalty_flybuys_account_dim fa
-    ON stl.dw_loyalty_flybuys_account_id = fa.dw_loyalty_flybuys_account_id
-INNER JOIN dss_private_design.adhoc.diy_proficiency_audience diy
-    ON fa.flybuys_membership_number_hash = diy.household
--- Join relevant range
-LEFT JOIN {table_name} ir ON 1=1
-    AND i.item_number = ir.item_number
-    AND i.country_code = ir.country_code
-WHERE 1=1
-    AND stl.sales_reporting_include_ind = TRUE
-    AND stl.country_code = 'AU'
-    AND stl.transaction_date BETWEEN {start_date} AND {end_date}
-    AND stl.customer_type_code = 'Consumer'
-GROUP BY
-    i.item_class_name,
-    diy.proficiency_group
+    (sl.segment_sales / NULLIF(tl.total_sales, 0))
+        / NULLIF(st.total_seg_sales / NULLIF(ovr.overall_sales, 0), 0) AS sales_index,
+
+    (sl.segment_quantity / NULLIF(tl.total_quantity, 0))
+        / NULLIF(st.total_seg_quantity / NULLIF(ovr.overall_quantity, 0), 0) AS quantity_index,
+
+    (sl.segment_customers / NULLIF(tl.total_customers, 0))
+        / NULLIF(st.total_seg_customers / NULLIF(ovr.overall_customers, 0), 0) AS customer_index
+
+FROM seg_level sl
+JOIN total_level tl
+    ON sl.{level} = tl.{level}
+JOIN seg_total st
+    ON sl.proficiency_group = st.proficiency_group
+JOIN overall ovr
+    ON 1=1
 ORDER BY
-    i.item_class_name,
-    diy.proficiency_group
-;
+    sl.{level},
+    sl.proficiency_group;
--- NZ pest control & growing media/sql/4-commercial-bot.sql
+++ NZ pest control & growing media/sql/4-commercial-bot.sql
@@ -1,49 +1,128 @@
--- Commercial BOT
+-- Commercial Segment Indexing by {level} and Industry Segment
+
+WITH comm_seg AS (
+    SELECT
+        ca.dw_commercial_account_id,
+        cisrg.commercial_industry_segment_report_group_code AS industry_segment
+    FROM bdwprd_cds.commercial.commercial_account_fct ca
+    JOIN bdwprd_cds.commercial.commercial_industry_segment_report_group_dim cisrg
+        ON ca.dw_commercial_industry_segment_report_group_id = cisrg.dw_commercial_industry_segment_report_group_id
+),
+
+-- Step 1: All transactions for items in range
+range_trx AS (
+    SELECT
+        i.{level},
+        cs.industry_segment,
+        stl.dw_commercial_account_id AS customer_id,
+        stl.total_exclude_gst_amount AS sales,
+        stl.item_quantity AS quantity
+    FROM bdwprd_cds.sales.sales_transaction_line_fct stl
+    JOIN bdwprd_de.ia_merch_de.{table_name} ir
+        ON stl.dw_item_id = ir.dw_item_id
+        AND stl.country_code = ir.country_code
+    JOIN bdwprd_cds.item.item_dim i
+        ON stl.dw_item_id = i.dw_item_id
+    JOIN comm_seg cs
+        ON stl.dw_commercial_account_id = cs.dw_commercial_account_id
+    WHERE 1=1
+        AND stl.sales_reporting_include_ind = TRUE
+        AND stl.country_code = '{country}'
+        AND stl.transaction_date BETWEEN {start_date} AND {end_date}
+        AND stl.customer_type_code = 'Commercial'
+),
+
+-- Totals per {level} × industry segment
+seg_level AS (
+    SELECT
+        {level},
+        industry_segment,
+        SUM(sales) AS segment_sales,
+        SUM(quantity) AS segment_quantity,
+        COUNT(DISTINCT customer_id) AS segment_customers
+    FROM range_trx
+    GROUP BY {level}, industry_segment
+),
+
+-- Totals per {level}
+total_level AS (
+    SELECT
+        {level},
+        SUM(sales) AS total_sales,
+        SUM(quantity) AS total_quantity,
+        COUNT(DISTINCT customer_id) AS total_customers
+    FROM range_trx
+    GROUP BY {level}
+),
+
+-- Totals per industry segment
+seg_total AS (
+    SELECT
+        industry_segment,
+        SUM(sales) AS total_seg_sales,
+        SUM(quantity) AS total_seg_quantity,
+        COUNT(DISTINCT customer_id) AS total_seg_customers
+    FROM range_trx
+    GROUP BY industry_segment
+),
+
+-- Overall totals
+overall AS (
+    SELECT
+        SUM(sales) AS overall_sales,
+        SUM(quantity) AS overall_quantity,
+        COUNT(DISTINCT customer_id) AS overall_customers
+    FROM range_trx
+)
+
+-- Final output with indexes
 SELECT
-    i.item_class_name,
-    cisrg.commercial_industry_segment_report_group_code,
-    -- Item range metrics
-    SUM(IFF(ir.item_number IS NOT NULL, stl.total_exclude_gst_amount, NULL)) AS range_sales,
-    SUM(IFF(ir.item_number IS NOT NULL, stl.item_quantity, NULL)) AS range_quantity,
-    COUNT(DISTINCT IFF(ir.item_number IS NOT NULL, stl.dw_commercial_account_id, NULL)) AS range_customers,
-    -- Item range shares
-    range_sales / SUM(range_sales) OVER () AS range_sales_share,
-    range_quantity / SUM(range_quantity) OVER () AS range_quantity_share,
-    range_customers / SUM(range_customers) OVER () AS range_customer_share,
-    -- Overall metrics
-    SUM(stl.total_exclude_gst_amount) AS overall_sales,
-    SUM(stl.item_quantity) AS overall_quantity,
-    COUNT(DISTINCT stl.dw_commercial_account_id) AS overall_customers,
-    -- Overall shares
-    overall_sales / SUM(overall_sales) OVER () AS overall_sales_share,
-    overall_quantity / SUM(overall_quantity) OVER () AS overall_quantity_share,
-    overall_customers / SUM(overall_customers) OVER () AS overall_customer_share,
+    sl.{level},
+    sl.industry_segment,
+
+    sl.segment_sales,
+    sl.segment_quantity,
+    sl.segment_customers,
+
+    tl.total_sales,
+    tl.total_quantity,
+    tl.total_customers,
+
+    st.total_seg_sales,
+    st.total_seg_quantity,
+    st.total_seg_customers,
+
+    ovr.overall_sales,
+    ovr.overall_quantity,
+    ovr.overall_customers,
+
+    -- Shares in {level}
+    sl.segment_sales / NULLIF(tl.total_sales, 0) AS segment_sales_share,
+    sl.segment_quantity / NULLIF(tl.total_quantity, 0) AS segment_quantity_share,
+    sl.segment_customers / NULLIF(tl.total_customers, 0) AS segment_customer_share,
+
+    -- Shares overall
+    st.total_seg_sales / NULLIF(ovr.overall_sales, 0) AS overall_sales_share,
+    st.total_seg_quantity / NULLIF(ovr.overall_quantity, 0) AS overall_quantity_share,
+    st.total_seg_customers / NULLIF(ovr.overall_customers, 0) AS overall_customer_share,
+
     -- Indexes
-    range_sales_share / overall_sales_share AS sales_index,
-    range_quantity_share / overall_quantity_share AS quantity_index,
-    range_customer_share / overall_customer_share AS customer_index
-FROM bdwprd_cds.sales.sales_transaction_line_fct stl
-INNER JOIN bdwprd_cds.item.item_dim i
-    ON stl.dw_item_id = i.dw_item_id
-INNER JOIN bdwprd_cds.location.location_dim l
-    ON stl.dw_location_id = l.dw_location_id
-INNER JOIN bdwprd_cds.commercial.commercial_account_fct ca
-    ON stl.dw_commercial_account_id = ca.dw_commercial_account_id
-INNER JOIN bdwprd_cds.commercial.commercial_industry_segment_report_group_dim cisrg
-    ON ca.dw_commercial_industry_segment_report_group_id = cisrg.dw_commercial_industry_segment_report_group_id
--- Join relevant range
-LEFT JOIN {table_name} ir ON 1=1
-    AND i.item_number = ir.item_number
-    AND i.country_code = ir.country_code
-WHERE 1=1
-    AND stl.sales_reporting_include_ind = TRUE
-    AND stl.country_code = 'AU'
-    AND stl.transaction_date BETWEEN {start_date} AND {end_date}
-    AND stl.customer_type_code = 'Commercial'
-GROUP BY
-    i.item_class_name,
-    cisrg.commercial_industry_segment_report_group_code
+    (sl.segment_sales / NULLIF(tl.total_sales, 0))
+        / NULLIF(st.total_seg_sales / NULLIF(ovr.overall_sales, 0), 0) AS sales_index,
+
+    (sl.segment_quantity / NULLIF(tl.total_quantity, 0))
+        / NULLIF(st.total_seg_quantity / NULLIF(ovr.overall_quantity, 0), 0) AS quantity_index,
+
+    (sl.segment_customers / NULLIF(tl.total_customers, 0))
+        / NULLIF(st.total_seg_customers / NULLIF(ovr.overall_customers, 0), 0) AS customer_index
+
+FROM seg_level sl
+JOIN total_level tl
+    ON sl.{level} = tl.{level}
+JOIN seg_total st
+    ON sl.industry_segment = st.industry_segment
+JOIN overall ovr
+    ON 1=1
 ORDER BY
-    i.item_class_name,
-    cisrg.commercial_industry_segment_report_group_code
-;
+    sl.{level},
+    sl.industry_segment;
