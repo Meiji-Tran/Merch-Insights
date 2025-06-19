c52ae12b921cb1f4b90ee098fd4c34e25b960e2e
Meiji-Tran <meiji.tran1@bunnings.com.au>
2025-06-19 09:08:53 +0000

    Association data generate sql

--- Category Health/sql/0-item-range.sql
+++ Category Health/sql/0-item-range.sql
@@ -4,7 +4,7 @@ CREATE OR REPLACE TABLE bdwprd_de.ia_merch_de.{table_name} AS (
         item_number,
         country_code,
         dw_item_id
-    FROM bdwprd_cds.item.item_dim
+    FROM bdwprd_cds.item.item_dim -- Can be custom list from bdwprd_de.ia_merch_de instead
     WHERE 1=1
         {filter}
     )
